f1a7e19555f49eff0fce3825d8436373af728931
jyang87 <jyang87@hotmail.com>
2018-05-13 19:11:53 +0800

    2018.05.13 19:10

--- Food Tracker/Food Tracker/ViewController.swift
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
     /**
     override func didReceiveMemoryWarning() {
@@ -20,7 +26,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     */
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = nameTextField.text
+    }
 
-
+    //MARK: Action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+       
+        let nowDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = formatter.string(from: nowDate)
+        
+        date.text = dateString
+        
+        //mealNameLabel.text = nameTextField.text
+ 
+    }
+    
 }
 
