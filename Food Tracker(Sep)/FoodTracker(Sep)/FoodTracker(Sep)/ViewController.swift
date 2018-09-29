c6309f17e4c6afa29f27328e41bfc7c6349b06ca
jyang87 <jyang87@hotmail.com>
2018-09-29 16:23:41 +0800

    Create

---
+++ Food Tracker(Sep)/FoodTracker(Sep)/FoodTracker(Sep)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker(Sep)
+//
+//  Created by JiangYang on 2018/9/29.
+//  Copyright © 2018 edu.self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
