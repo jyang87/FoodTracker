0f9f54756c52f18ba3af14a37805b308cf6f1dd9
jyang87 <jyang87@hotmail.com>
2018-09-30 15:04:52 +0800

    2018-09-30

--- Food Tracker(Sep)/FoodTracker(Sep)/FoodTracker(Sep)/ViewController.swift
+++ Food Tracker(Sep)/FoodTracker(Sep)/FoodTracker(Sep)/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    //MARK: Properties
+   
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var iOSVersion: UILabel!
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       nameTextField.delegate = self
     }
-
-
+ //MARK: Action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+        
+        let sysdate = NSDate()
+        let timeDateFormate = DateFormatter()
+        timeDateFormate.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let strDate = timeDateFormate.string(from: sysdate as Date)
+        
+        let iosVersion = UIDevice.current.systemVersion 
+        
+        timeLabel.text = strDate
+        iOSVersion.text = "iOS Version : " + iosVersion
+    }
+    
+    //MARK: TextDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide keyboard
+       
+        return textField.resignFirstResponder()
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = nameTextField.text
+    }
 }
 
